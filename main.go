e83dc54b469482b55a3e4a8e6af1f0eddaddb6e0
Rafael Chargel <rchargel@gmail.com>
2023-10-11 21:52:36 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", showIndexPage)
+	router.GET("/article/view/:article_id", getArticle)
+
+	router.Run()
+}
